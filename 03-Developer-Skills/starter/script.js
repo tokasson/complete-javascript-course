21979c0e898095652f443b24ffcf353ec93efc3d
tokasson <42979720+tokasson@users.noreply.github.com>
2021-02-19 08:57:49 +0100

    update

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printFC = (tempFC) => {
+    let text = '...';
+    for (let i = 0; i < tempFC.length; i++) {
+        text += ' ' + tempFC[i] + '°C in ' + (i+1) + ' days ...'
+    }
+    return text;
+};
+
+const test1 = [17,21,23];
+const test2 = [12,5,-5,0,4];
+console.log(printFC(test1));
+console.log(printFC(test2));
